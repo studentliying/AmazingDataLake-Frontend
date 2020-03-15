e9c8df731f9f62e881923980679f5ceffc4a7277
studentliying <540906886@qq.com>
2020-03-15 16:46:41 +0800

    add fetch

---
+++ src/Utils/Constant.js
@@ -0,0 +1,11 @@
+/**
+ * Created by 励颖 on 2020/3/15.
+ */
+const prefix = "http://";
+const domain = "localhost";
+const port = "8080";
+const server = prefix + domain + ":" + port;
+
+module.exports ={
+  server
+};
