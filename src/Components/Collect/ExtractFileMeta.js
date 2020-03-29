1e023205c296c0c8710a1e3d203712827f6d7f09
studentliying <540906886@qq.com>
2020-03-29 21:17:41 +0800

    show result of knowledge extraction

--- src/Components/Collect/ExtractFileMeta.js
+++ src/Components/Collect/ExtractFileMeta.js
@@ -2,7 +2,7 @@
  * Created by 励颖 on 2020/3/12.
  */
 import React from 'react';
-import {Checkbox, Button, Card, Row, Col} from 'antd';
+import {Checkbox, Button, Card, Row, Col, Spin, Alert, Result} from 'antd';
 import {server} from '../../Utils/Constant';
 
 const CheckboxGroup = Checkbox.Group;
@@ -12,6 +12,8 @@ class ExtractFileMeta extends React.Component {
     super(props);
     this.state = {
       checkedList: [],
+      spinning: false,
+      result: false
     };
   }
 
@@ -20,32 +22,43 @@ class ExtractFileMeta extends React.Component {
     temp_list[key] = checkedList;
     this.setState({
       checkedList: temp_list,
+    },()=>{
+      console.log(this.state.checkedList);
     });
-    console.log("checkedList: ", checkedList);
-    console.log("key: ", key);
+
   };
 
 
   handleButtonClick = () => {
     let body = {
-      "body_header": this.state.checkedList,
+      "body_header": this.state.checkedList.slice(0,this.props.parentFilenames.length),
       "body_data": this.props.parentData,
       "body_filename": this.props.parentFilenames
     };
-    fetch(server + "/rdf/from_csv" , {
-      method: 'POST',
-      body: JSON.stringify(body),
-      mode: 'cors',
-      header:{
-        contentType:"application/json"
-      }
+    this.setState({
+      spinning: true,
+    }, ()=>{
+      fetch(server + "/rdf/from_csv" , {
+        method: 'POST',
+        body: JSON.stringify(body),
+        mode: 'cors',
+        header:{
+          contentType:"application/json"
+        }
+      })
+          .then((response) => {
+            console.log("response: ", response);
+            if (response.status === 200) {
+              this.setState({
+                result: true
+              })
+            }
+          })
+          .then(function (result) {
+            console.log("result: ", result);
+          })
     })
-      .then(function (response) {
-        console.log("response: ", response);
-      })
-      .then(function (result) {
-        console.log("result: ", result);
-      })
+
   };
 
   handleMultiCard = () =>{
@@ -77,18 +90,38 @@ class ExtractFileMeta extends React.Component {
         <Card title="元数据抽取"
               headStyle={{backgroundColor:"#d6e4ff", fontSize:'20px', fontWeight:'700'}}
               bodyStyle={{backgroundColor:"#f0f5ff"}}>
-          <Row>
-            {
-              this.props.parentHeader.length > 0 ? this.handleMultiCard() : null
-            }
-          </Row>
-          <br/>
-          <Row>
-            {
-              this.props.parentHeader.length > 0 ?
-                  <Button onClick={this.handleButtonClick}>确定抽取</Button> : null
-            }
-          </Row>
+          {
+              this.state.result === true ?
+                  <Result
+                      status="success"
+                      title="知识抽取已完成"
+                      extra={[
+                        <Button type="primary" key="console">
+                          前往查看抽取结果
+                        </Button>,
+                      ]}
+                  />
+                  :
+                  <div>
+                    <Row>
+                      {
+                        this.props.parentHeader.length > 0 ? this.handleMultiCard() : null
+                      }
+                    </Row>
+                    <br/>
+                    <Row>
+                      {
+                        this.props.parentHeader.length > 0 ?
+                            <div>
+                              <Button onClick={this.handleButtonClick}>确定抽取</Button>
+                              &nbsp;&nbsp;&nbsp;
+                              <Spin tip="知识抽取中..." spinning={this.state.spinning}/>
+                            </div>
+                              : null
+                      }
+                    </Row>
+                  </div>
+              }
         </Card>
     )}
 }
